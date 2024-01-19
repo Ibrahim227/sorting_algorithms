0ecd232b89fa5ab796b76f21344676ab9aa433b5
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-19 12:06:45 -0800

    add 0-buble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+* bubble_sort - sorts an array of int in ascending order
+* @array: int to be printed
+* @size: size of integer
+* Return: array
+*/
+void bubble_sort(int *array, size_t size)
+{
+    int i;
+    int j;
+}
