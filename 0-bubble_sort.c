b74e37377dd6b9aad9ed9342f8cbca3805fbc568
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-19 13:36:00 -0800

    update 0-bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,10 +10,12 @@ void bubble_sort(int *array, size_t size)
 {
 	int i, j;
 	bool swapped;
+	int arr[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7}
 
 	for (i = 0; i < size - 1; i++)
-		swapped = false;
+	swapped = false;
 		for (j = 0; j < size - i - 1; j++)
 			{
-				if (arr[j] > arr[j + 1]) {}}
+				if (arr[j] > arr[j + 1])
+			}
 }
