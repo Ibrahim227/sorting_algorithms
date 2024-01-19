744824eae145844283f67aa5719e20b9daca85f6
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-19 14:00:47 -0800

    add 1-insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+
+/**
+* insertion_sort_list - sorts  a doubly ;inked list of int
+* @list: list to be sorted
+* Return: list
+*/
+void insertion_sort_list(listint_t **list)
+{
+}
