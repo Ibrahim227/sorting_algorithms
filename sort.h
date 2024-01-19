d9815f39147c3087c4641fd7429e5142016b9658
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-19 12:42:40 -0800

    update prototype

--- sort.h
+++ sort.h
@@ -49,17 +49,7 @@ void print_list(const listint_t *list)
     int i;
 
     i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
-}
-
+ 
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
