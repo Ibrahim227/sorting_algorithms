e7e4db8b89bd1576415b9a5ad1bf79f9a5103744
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-19 14:01:22 -0800

    add 2-selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+* selection_sort - sorts an array of int
+* @array: array to be sorted
+* @size: size of the array
+* Return: sorted list
+*/
+void selection_sort(int *array, size_t size)
+{
+}
