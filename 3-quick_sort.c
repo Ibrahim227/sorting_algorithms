f3d842412af6d40f2b83acc65dc377bf104bea47
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-19 14:02:15 -0800

    add 3_quick_sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+* void quick_sort - sorts an array of int in ascending order
+* @array: array to be sorted
+* @size: size of the aray
+* Return: sorted list of array
+*/
+void quick_sort(int *array, size_t size)
+{
+}
