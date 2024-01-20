d3d90e0696f95a7d9a95618c52e2e51699134b72
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-20 14:48:27 -0800

    update 2-selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,18 @@
 #include "sort.h"
 
 /**
+* swap - swap 2 int
+* @xp: int
+* @yp: int
+* Return: array
+*/
+void swap(int *xp, int *yp) 
+{ 
+	int temp = *xp; 
+	*xp = *yp; 
+	*yp = temp; 
+}
+/**
 * selection_sort - sorts an array of int
 * @array: array to be sorted
 * @size: size of the array
