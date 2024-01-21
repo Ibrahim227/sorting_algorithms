84938065337eed80a28d20bc7ef8a145d799ba21
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-21 08:47:04 -0800

    update 100-shell_sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,13 +12,13 @@ void shell_sort(int *array, size_t size)
 
 	for (gap = size/2; gap > 0; gap /= 2)
 	{
-		for (int i = gap; i < size; i+= 1)
+		for (int i = gap; i < size; i += 1)
 		{
 			int temp = array[i];
 
 			int j;
 			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-				array[j] = array[j - gap]
+				array[j] = array[j - gap];
 			array[j] = temp;
 		}
 	}
