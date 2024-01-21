802e37c36294a20c6e4807e9933c88337de6bff1
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-21 08:36:19 -0800

    add 100-shell_sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+* shell_sort - sorts an array of integers ins asc order
+* @array: array
+* @size: size of the array
+* Return: array
+*/
+void shell_sort(int *array, size_t size)
+{
+
+}
