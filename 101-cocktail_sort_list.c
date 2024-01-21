d7fcc045bf1510a58e532d8f3c0448103e27dbad
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-21 09:11:49 -0800

    update 101-cocktail_sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+ * swap - Swap two integers in an array.
+ * @c: The first integer to swap.
+ * @d: The second integer to swap.
+* Return: sorted list
+ */
+void swap(int *c, int *d)
+{
+        int tmp;
+
+        tmp = *c;
+        *c = *d;
+        *d = tmp;
+}
+
+/**
 * cocktail_sort_list - sort a doubly linked list on int in asc order
 * @list: list
 * Return: list
